7fe5b8241fde72a81bd159efe091acd17dbf25cd
Elina Sankila <sankilaelina@gmail.com>
2024-03-27 15:51:09 +0200

    week2 t2 done

--- week2/t2.php
+++ week2/t2.php
@@ -1,10 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     print_r($_POST);
-    if(isset($_POST['remember-me'])) {
+    if (isset($_POST['remember-me'])) {
         setcookie('username', $_POST['username'], time() + 60 * 60 * 24 * 30);
-    }
-    else {
+    } else {
         setcookie('username', '', time() - 3600);
     }
 }
@@ -12,10 +11,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <label for="username">Username</label>
-    <input type="text" name="username" id="username">
+    <input <?php if (isset($_COOKIE['username'])) {
+        echo 'value="' . $_COOKIE['username'] . '"';
+    } ?>
+            type="text" name="username" id="username">
 
     <label for="remember-me">Remember me</label>
-    <input type="checkbox" name="remember-me" id="remember-me">
+    <input <?php if (isset($_COOKIE['username'])) {
+        echo 'checked';
+    } ?>
+            type="checkbox" name="remember-me" id="remember-me">
 
     <button type="submit">Submit</button>
 </form>
